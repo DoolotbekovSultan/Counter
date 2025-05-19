99a1d3e3d0d0102c3e5baa695ca2e059969681c2
DoolotbekovSultan <doolotbekov_sultan@icloud.com>
2025-05-19 17:25:53 +0600

    fix

--- app/src/main/java/com/sultan/counter/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/sultan/counter/presentation/activity/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.sultan.counter.presentation.activity
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.sultan.counter.databinding.ActivityMainBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-    private val viewModel by viewModels<MainViewModel>()
+    private val viewModel : MainViewModel by viewModel()
+    //private val viewModel by lazy { ViewModelProvider(this)[MainViewModel::class.java] }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
